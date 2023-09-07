be91d6961d90d9ac3e5eb4286f0cd38f23426001
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-09-07 18:29:49 +0530

    dynamic data passed to issue

--- parse-json.js
+++ parse-json.js
@@ -1,29 +1,30 @@
-const inputData = {
-    "repository-name": {
-      "title": "Repository name",
-      "content": ["first-repo"],
-      "text": "first-repo"
-    },
-    "enable-wiki": {
-      "title": "Enable Wiki?",
-      "content": [],
-      "text": "*   [x] Enable Wiki",
-      "list": [{"checked": true, "text": "Enable Wiki"}]
-    },
-    "enable-projects": {
-      "title": "Enable Projects?",
-      "content": [],
-      "text": "*   [x] Enable Projects",
-      "list": [{"checked": true, "text": "Enable Projects"}]
-    },
-    "enable-issues": {
-      "title": "Enable Issues?",
-      "content": [],
-      "text": "*   [x] Enable Issues",
-      "list": [{"checked": false, "text": "Enable Issues"}]
-    }
-  };
-  
+// const inputData = {
+//     "repository-name": {
+//       "title": "Repository name",
+//       "content": ["first-repo"],
+//       "text": "first-repo"
+//     },
+//     "enable-wiki": {
+//       "title": "Enable Wiki?",
+//       "content": [],
+//       "text": "*   [x] Enable Wiki",
+//       "list": [{"checked": true, "text": "Enable Wiki"}]
+//     },
+//     "enable-projects": {
+//       "title": "Enable Projects?",
+//       "content": [],
+//       "text": "*   [x] Enable Projects",
+//       "list": [{"checked": true, "text": "Enable Projects"}]
+//     },
+//     "enable-issues": {
+//       "title": "Enable Issues?",
+//       "content": [],
+//       "text": "*   [x] Enable Issues",
+//       "list": [{"checked": false, "text": "Enable Issues"}]
+//     }
+//   };
+const inputData = require('./issue.json');
+
   const extractedData = {
     "repository-name": inputData["repository-name"]["text"],
     "enable-wiki": inputData["enable-wiki"]["list"][0]["checked"],
