8581a7989f75b382cb032b2f1322148629cdc7c0
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-09-07 19:02:46 +0530

    writing to json file

--- parse-json.js
+++ parse-json.js
@@ -31,8 +31,6 @@ const inputData = require('./issue.json');
     "has_projects": inputData["enable-projects"]["list"][0]["checked"],
     "has_issues": inputData["enable-issues"]["list"][0]["checked"]
   };
-  //return extractedData to actions
-  return extractedData;
-
-  //console.log(extractedData);
+// Write the extracted data to a JSON file
+fs.writeFileSync('extracted-data.json', JSON.stringify(extractedData, null, 2));
   
